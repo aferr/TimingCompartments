62f5f244fa12bad325b73366561dc53875e61a56
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2015-01-03 14:27:42 -0500

    pfault test

--- tools/first_diff.rb
+++ tools/first_diff.rb
@@ -1,30 +1,31 @@
 #!/bin/ruby
 require 'colored'
 
-traceregex = /\w* system\.cpu0 T0(.*)$/
+traceregex = /\w* system\..*cpus?0 T0(.*)$/
 
 if __FILE__ == $0
   f1 = File.open(ARGV[0])
   f2 = File.open(ARGV[1])
   f1enum = Enumerator.new do |y|
-    f1p = f1.each_line.lazy
+    f1p = f1.each_line
     loop do
       n = f1p.next until n =~ traceregex
       y << n
     end
-  end.lazy
+  end
   f2enum = Enumerator.new do |y|
-    f2p = f2.each_line.lazy
+    f2p = f2.each_line
     loop do
       n = f2p.next until n =~ traceregex
       y << n
     end
-  end.lazy
+  end
 
   loop do
     break if lambda do |x, y|
       ins = ->(z){ z.match(traceregex)[1] }
-      (puts "#{x}!=#{y}"; true) if ins.call(x) != ins.call(y)
+      puts "#{x} #{y}".blue
+      (puts "#{x}!=#{y}".red; true) if ins.call(x) != ins.call(y)
     end.call(f1enum.next, f2enum.next)
   end
 
